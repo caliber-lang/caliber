8549d13434945287136de751bfcc92358ec14e3b
confucius40 <git@mail.katon15.xyz>
2026-08-13 16:50:17 +0200

    fix type shit

--- suic/_tests/test-lexer.rkt
+++ suic/_tests/test-lexer.rkt
@@ -1,10 +1,19 @@
 #lang racket
 
-(require "../lexer.rkt")
+(require "lexer.rkt")
 
+;; test tokenization
 (define test-sui
   "def main =
      var x <- 5
      print x")
 
-(display (tokenize test-sui))
+;; convert string to input port
+(define (tokenize-string str)
+  (tokenize (open-input-string str)))
+
+;; run test
+(let loop ([tok (tokenize-string test-sui)])
+  (unless (eq? (token-type tok) 'eof)
+    (printf "~a: ~a~n" (token-type tok) (token-value tok))
+    (loop (tokenize-string test-sui))))
